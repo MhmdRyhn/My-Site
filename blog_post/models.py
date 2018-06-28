1a79ec308f383380c46b73d4be05e13323169780
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2018-06-28 18:32:56 +0600

    Formatted text in blog_post/views.py & blog_post/models.py

--- blog_post/models.py
+++ blog_post/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500, verbose_name='Title')
+    post = models.TextField(verbose_name='Post')
+    post_time = models.DateTimeField()
+    update_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.post_time = timezone.now()
+        self.update_time = timezone.now()
+        return super(Post, self).save(*args, **kwargs)
+
+
+
+
+
--- blog_post/views.py
+++ blog_post/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import CreatePostForm, EditPostForm
+from .models import Post
 
-# Create your views here.
+
+@login_required
+def create_post(request):
+    form = CreatePostForm(request.POST or None)
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.user = request.user
+        post.save()
+        return redirect('profile')
+
+    context = {
+        'form': form,
+        'user': request.user
+    }
+    return render(request, 'blog_post/new_post.html', context)
+
+
+@login_required
+def edit_post(request, username, post_id):
+    other_user = User.objects.get(username=username)
+    post = Post.objects.get(user=other_user.pk, pk=post_id)
+
+    form = EditPostForm(data=request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('single_post')
+
+    context = {
+        'form': form,
+        'user': request.user,
+        'other_user': other_user,
+        'post': post
+    }
+    if request.user.username == username:
+        context['is_same_user'] = True
+    else:
+        context['is_same_user'] = False
+    return render(request, 'blog_post/edit_post.html', context)
+
+
+@login_required
+def delete_post(request, username, post_id):
+    if request.user.username == username:
+        Post.objects.get(pk=post_id).delete()
+        return redirect('home')
+    else:
+        pass
+
+
+def view_post(request, username, post_id):
+    other_user = User.objects.get(username=username)
+    single_post = Post.objects.get(user=other_user.pk, pk=post_id)
+
+    context = {
+        'user': request.user,
+        'other_user': other_user,
+        'post': single_post
+    }
+    if request.user.username == username:
+        context['is_same_user'] = True
+    else:
+        context['is_same_user'] = False
+    return render(request, 'blog_post/single_post.html', context)
+
+
+def view_all_post(request, username):
+    # print('User:', User.objects.get(username=username).pk)
+    other_user = User.objects.get(username=username)
+    post = Post.objects.filter(user=other_user.pk)
+    context = {
+        'user': request.user,
+        'other_user': other_user,
+        'all_post': post
+    }
+    return render(request, 'blog_post/all_post.html', context)
+
+
+
+
+
