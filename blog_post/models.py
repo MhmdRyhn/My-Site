da5226fe0aef8512676bf04e504155a4339e8145
Mahmood Al Rayhan <rayhan.cse12@gmail.com>
2018-07-05 23:29:21 +0600

    Created model for Comment and PostEditHistory

--- blog_post/models.py
+++ blog_post/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     post = models.TextField(verbose_name='Post')
     post_time = models.DateTimeField()
     update_time = models.DateTimeField()
+    exists = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
@@ -16,8 +17,29 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.id:
             self.post_time = timezone.now()
+            # self.exists = True
         self.update_time = timezone.now()
         return super(Post, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField(verbose_name='Comment')
+    comment_time = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        self.comment_time = timezone.now()
+        return super(Comment, self).save(*args, **kwargs)
+
+
+class PostEditHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    body = models.TextField()
+    edit_time = models.DateTimeField()
+
 
 
 
